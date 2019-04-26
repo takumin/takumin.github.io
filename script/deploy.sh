13a8a5632930b3681173b6584ca7efe81f9b6036
takumin <takumiiinn@gmail.com>
2019-04-27 01:38:14 +0900

    update script

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,37 +5,11 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo
 
-# Go To Public folder
-cd public
-
 # Add changes to git.
-git add -A
+git -C public add -A
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]; then
-  msg="$1"
-fi
-git commit -m "$msg"
+git -C public commit -m "rebuilding site $(date)"
 
 # Push source and build repos.
-git push origin master
-
-# Come Back up to the Project Root
-cd ..
-
-# Pulling changes
-git pull origin master
-
-# Add changes to git.
-git add public
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]; then
-  msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
+git -C public push origin master
