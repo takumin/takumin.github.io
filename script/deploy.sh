b0c4baff97a5a70e28ebe843141c4dfe027c0fcf
takumin <takumiiinn@gmail.com>
2020-04-08 22:52:26 +0900

    cleanup old files

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,6 +5,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Checkout master branch.
 git -C public checkout master
 
+# Cleanup old files
+find public -mindepth 1 -not -name '.*' -print0 | xargs -0 rm -fr
+
 # Build the project.
 hugo
 
